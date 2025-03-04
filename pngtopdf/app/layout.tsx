67d95ebbf768490d3ad09cb1a3e061aff3803e31
walid <itswalidbensaid@gmail.com>
2025-03-04 16:59:16 +0200

    icons

--- pngtopdf/app/layout.tsx
+++ pngtopdf/app/layout.tsx
@@ -73,6 +73,11 @@ export const metadata: Metadata = {
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }, // Optional: 16x16 favicon
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }, // Optional: 32x32 favicon
     ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+
+
   },
 
 
@@ -103,6 +108,8 @@ export default function RootLayout({
         {/* End Google Tag Manager */}
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
